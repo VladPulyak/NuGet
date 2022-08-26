490cae730c20f2b8e512580519446cd8a5071b33
VladPulyak <vlad.pulyak03@mail.ru>
2022-08-26 21:36:24 +0300

    Initial commit

---
+++ NuGet Practise/NuGet Practise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NuGet_Practise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
